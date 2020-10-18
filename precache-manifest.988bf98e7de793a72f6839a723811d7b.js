50189b86bb15fe869a7f9e559a86a2e01776df9a
asadalibest1 <asadalibest1@gmail.com>
2020-10-18 15:42:54 +0500

    Updates

---
+++ precache-manifest.988bf98e7de793a72f6839a723811d7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08c05d4c7006f892086d057ef833c5b",
+    "url": "/Web-Animation-Api-with-Hooks/index.html"
+  },
+  {
+    "revision": "a6744218d7e154a86c4a",
+    "url": "/Web-Animation-Api-with-Hooks/static/css/main.0c79e7a2.chunk.css"
+  },
+  {
+    "revision": "21480abbcfb6098a9744",
+    "url": "/Web-Animation-Api-with-Hooks/static/js/2.1dd4a992.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Web-Animation-Api-with-Hooks/static/js/2.1dd4a992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6744218d7e154a86c4a",
+    "url": "/Web-Animation-Api-with-Hooks/static/js/main.2c1caa86.chunk.js"
+  },
+  {
+    "revision": "70f300e2a716cf56ecbe",
+    "url": "/Web-Animation-Api-with-Hooks/static/js/runtime-main.1c2b0769.js"
+  },
+  {
+    "revision": "428b9f860e09dcdaf336a6b201ce991b",
+    "url": "/Web-Animation-Api-with-Hooks/static/media/github.428b9f86.png"
+  }
+]);
